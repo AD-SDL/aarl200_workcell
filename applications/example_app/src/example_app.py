7722b19c10266b05535e9d2933fcc22c8de3fdc0
Ryan D. Lewis <ryan.lewis@anl.gov>
2024-10-25 16:47:21 -0500

    adding other thhings

--- applications/example_app/src/example_app.py
+++ applications/example_app/src/example_app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Example experiment application that uses the WEI client to run a workflow."""
 
-import json
 from pathlib import Path
 
 from wei import ExperimentClient
@@ -10,30 +9,47 @@ from wei import ExperimentClient
 def main() -> None:
     """Runs an example WEI workflow"""
     # This defines the Experiment object that will communicate with the WEI server
-    exp = ExperimentClient("localhost", "8000", "Example_Program")
+    exp = ExperimentClient("localhost", "8000", "ARL200_Demo")
 
     wf_dir = (Path(__file__).parent.parent / "workflows").resolve()
 
-    wf_run = exp.start_run(
-        wf_dir / "open_door.workflow.yaml",
-    )
-    print(wf_run)
+    while True:
+        # wf_run = exp.start_run(
+        #     wf_dir / "open_door.workflow.yaml",
+        # )
 
-    # This runs the workflow
-    wf_run = exp.start_run(
-        wf_dir / "pick_tube_rack.workflow.yaml",
-    )
-    print(wf_run)
+        wf_run = exp.start_run(
+            wf_dir / "pick_tube_rack.workflow.yaml",
+        )
+        break
 
-    wf_run = exp.start_run(
-        wf_dir / "place_tube_rack_with_safe_approach.workflow.yaml",
-    )
-    print(wf_run)
+        # wf_run = exp.start_run(
+        #     wf_dir / "place_tube_rack_with_safe_approach.workflow.yaml",
+        # )
 
-    wf_run = exp.start_run(
-        wf_dir / "close_door.workflow.yaml",
-    )
-    print(wf_run)
+        # wf_run = exp.start_run(
+        #     wf_dir / "close_door.workflow.yaml",
+        # )
+
+        # wf_run = exp.start_run(
+        #     wf_dir / "move_to_middle.yaml",
+        # )
+
+        # wf_run = exp.start_run(
+        #     wf_dir / "open_door.workflow.yaml",
+        # )
+
+        # wf_run = exp.start_run(
+        #     wf_dir / "pick_tube_rack_with_safe_approach.workflow.yaml",
+        # )
+
+        # wf_run = exp.start_run(
+        #     wf_dir / "place_tube_rack.workflow.yaml",
+        # )
+
+        # wf_run = exp.start_run(
+        #     wf_dir / "close_door.workflow.yaml",
+        # )
 
 
 if __name__ == "__main__":
